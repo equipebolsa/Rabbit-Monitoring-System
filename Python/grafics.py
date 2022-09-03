98a3c4823fd9e6ab01f9bc0c966ddd0aaa9d0ec5
RafaRpso <rafaelalvesraposo@hotmail.com>
2022-09-03 20:56:03 -0300

    Dados pegando da maquina

--- Python/grafics.py
+++ Python/grafics.py
@@ -1,14 +1,18 @@
-import pymysql, matplotlib.pyplot as plt ,re,os, numpy as np    
+import pymysql, matplotlib.pyplot as plt ,re,os, numpy as np, psutil,asyncio, time, collections 
 from matplotlib.pyplot import figure
 from matplotlib.animation import FuncAnimation
-
-
-
 fig, (ax,ax1,ax2)= plt.subplots(1,3,gridspec_kw={'width_ratios': [0.5,0.5,0.5], 'height_ratios': [0.8]})
 
+program_open_time=[]
+cpu_y=[]
+ram_y=[]
+disk_y=[]
+async def att_date() :
+    program_open_time.append(os.popen('date').read().split()[4])
 
-def currentDate() : 
-    return   os.popen('date').read().split()[4]
+    await asyncio.sleep(1)
+    
+
 
 def config () :
     connection = pymysql.connect(host='localhost',
@@ -37,10 +41,23 @@ def select_data (component) :
         cursor.execute("SELECT idDisco, capacidadeDisco, espacoLivreDisco, espacoUsadoDisco, porcentagemUsoDisco, dataDisco FROM tbDisco ORDER BY dataDisco DESC  LIMIT 20")    
         return cursor.fetchall()
 
+def psutil_data(arr, component) : 
+    if(component.lower() == 'cpu') : 
+        return  round(psutil.cpu_freq().current *
+                         pow(2, 13) / psutil.cpu_freq().max, 2)
+    elif(component.lower() == 'ram') : 
+        return (round(psutil.virtual_memory().used / pow(1024,3),2) )
+    elif(component.lower() == 'disk') : 
+        return  (round(psutil.disk_usage('/').percent))
 
 def animate (i) :
 
-    cpu_full_data =  select_data('cpu')
+    asyncio.run(att_date())
+    global program_open_time
+
+    cursor.execute("SELECT * FROM tbDadosCpu ORDER BY dataCpu DESC LIMIT 20 ")
+        
+    cpu_full_data = cursor.fetchall()
     ram_full_data = select_data('ram')
     disk_full_data = select_data('disco')
 
@@ -51,27 +68,15 @@ def animate (i) :
     ram_full_data = handle_data(ram_full_data,"espacoLivreRam")
     ram_full_data = handle_data(ram_full_data,"espacoUsadoRam")
 
-
-    cpu_x = get_data(cpu_full_data, 'dataCpu')
-    cpu_y = get_data(cpu_full_data, 'freqAtualCpu' )
-
-    ram_x = get_data(ram_full_data,'dataRam')
-    ram_y = get_data(ram_full_data, 'espacoUsadoRam')
-
-    disk_x = get_data(disk_full_data, 'dataDisco')
-    disk_y = get_data(disk_full_data, 'porcentagemUsoDisco')
-
-    print(cpu_full_data[0]['dataCpu'])
-
-    plotCharts(cpu_x, cpu_y, 'cpu', 100)
-
-    plotCharts(ram_x,ram_y, 'ram',  ram_full_data[0]['capacidadeRam'])
-
-    plotCharts(disk_x, disk_y, 'disk', 100)
-
-
-
+    cpu_y.append(psutil_data(cpu_y, 'cpu'))
+    ram_y.append(psutil_data(ram_y,'ram'))
+    disk_y.append(psutil_data(disk_y, 'disk'))
     
+    plotCharts(program_open_time, cpu_y,'cpu', 100)
+    plotCharts(program_open_time,ram_y, 'ram',  ram_full_data[0]['capacidadeRam'] )
+    plotCharts(program_open_time, disk_y, 'disk', 100)
+    
+
 
 def get_data(arr, item): 
     new_arr = []
@@ -90,39 +95,49 @@ def label_font_size(ax):
 	    label.set_fontsize(7)
 
 
+   
+
+
 def plotCharts(x,y, component, limit) : 
     blue = '#6250e6'
     green ='#79e675'
     orange = '#f5b795'
+    ax.tick_params(# ticks along the top edge are off
+        labelbottom=False) 
+    
+    ax1.tick_params(# ticks along the top edge are off
+        labelbottom=False) 
+    ax2.tick_params(# ticks along the top edge are off
+        labelbottom=False) 
     if(component.lower() == 'cpu'): 
-        ax.cla()
         ax.set_ylim([0,limit])
         ax.set_ylabel("Uso % CPU")
         ax.set_title("USO CPU")
-        ax.set_xlabel("Dia/Hora/Segundo")
-        ax.grid('on')
+     
         ax.fill_between(x, 0, y,  color='#9086db')
         ax.plot(x,y,  color=blue)
-
+        ax.xaxis.set_ticks_position('none')
+         
     elif(component.lower() == 'ram'): 
       
         ax1.set_ylim([0,limit])
         ax1.set_ylabel("Uso % RAM")
-        ax1.set_xlabel("Dia/Hora/Segundo")
+
         ax1.fill_between(x, 0, y,  color=green)
         ax1.set_title("USO RAM")
-        ax1.grid('on')
+
         ax1.plot(x,y, color='green', label='Ram')
-        
+        ax1.xaxis.set_ticks_position('none')
     elif(component.lower() == 'disk'): 
        
         ax2.set_ylim([0,limit])
         ax2.set_ylabel("Uso % Disco")
-        ax2.set_xlabel("Dia/Hora/Segundo")
+
         ax2.set_title("USO DISCO")
         ax2.fill_between(x, 0, y,  color=orange)
-        ax2.grid('on')
+  
         ax2.plot(x,y, color='orange')
+        ax2.xaxis.set_ticks_position('none')
     label_font_size(ax)
     label_font_size(ax1)
     label_font_size(ax2)
