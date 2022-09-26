01631e851b2dfc08d45aefc2ab59f2d1b3bfa7aa
GustavoAntonio12 <gustavo.antonio@sptech.school>
2022-09-26 14:32:28 -0300

    AlteraçõesSIte

--- Site/app.js
+++ Site/app.js
@@ -1,35 +1,28 @@
-process.env.AMBIENTE_PROCESSO = "desenvolvimento";
-// process.env.AMBIENTE_PROCESSO = "producao";
+// process.env.AMBIENTE_PROCESSO = "desenvolvimento";
+process.env.AMBIENTE_PROCESSO = "producao";
 
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = 3333;
+var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
 
 var app = express();
 
 var indexRouter = require("./src/routes/index");
-var userRouter = require("./src/routes/user");
-var mapaRouter = require("./src/routes/mapa");
-var scoreRouter = require("./src/routes/score");
-var jogador = require("./src/routes/jogador");
-
-  var bodyParser = require('body-parser');
-
-  app.use(bodyParser.json({limit: '5000000mb'}));
-  app.use(bodyParser.urlencoded({limit: '5000000mb', extended: false}));
+var usuarioRouter = require("./src/routes/usuarios");
+var avisosRouter = require("./src/routes/avisos");
+var medidasRouter = require("./src/routes/medidas");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-
 
 app.use(cors());
 
 app.use("/", indexRouter);
-app.use("/user", userRouter);
-app.use("/mapa", mapaRouter);
-app.use("/score", scoreRouter);
-app.use("/jogador", jogador);
+app.use("/usuarios", usuarioRouter);
+app.use("/avisos", avisosRouter);
+app.use("/medidas", medidasRouter)
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
@@ -38,5 +31,3 @@ app.listen(PORTA, function () {
     \t\tSe "producao", você está se conectando ao banco REMOTO (SQL Server em nuvem Azure) \n
     \t\t\t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'`);
 });
-
-
