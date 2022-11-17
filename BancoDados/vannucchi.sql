53558226b3f95d8277551c6bc15b0c9db0f29513
GabrielVannucchi <gabriel.correa@sptech.school>
2022-11-17 20:42:39 -0300

    bd vannucchi

---
+++ BancoDados/vannucchi.sql
@@ -0,0 +1,32 @@
+drop database teste;
+create database teste;
+use teste;
+create table whitelist(
+	id int primary key auto_increment,
+    nome varchar(100)
+);
+create table blacklist(
+	id int primary key auto_increment,
+    nome varchar(100)
+);
+create table alertaProcesso(
+	id int primary key auto_increment,
+    nome varchar(100),
+    serialNumber varchar(45),
+    estado char(1),
+    datahora datetime,
+    CHECK (estado = 'y' or estado = 'n' or estado = 'w' or estado = 'd') -- aceito, recusado, aguardando, concluido
+);
+
+-- insert into alertaProcesso values (null, itemProcesso['name'], def getMachine_addr(), 'w', now);
+
+-- asduashdaushduashdhasd
+select * from whitelist;
+select * from blacklist;
+truncate blacklist;
+select * from alertaProcesso;
+truncate alertaProcesso;
+update alertaProcesso set estado = 'y' where id = 1;
+insert into blacklist values(null,'Tibia.exe');
+insert into blacklist values(null,'Spotify.exe');
+delete from whitelist where id = 156;
